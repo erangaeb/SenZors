fb27b2ae8cdf74721b2345cbf0dc1d45449b0c74
eranga bandara <erangaeb@gmail.com>
2014-02-11 23:52:45 +0530

    added cutom theam senzors

--- src/com/score/senzors/ui/SensorDetailsActivity.java
+++ src/com/score/senzors/ui/SensorDetailsActivity.java
@@ -1,9 +1,15 @@
 package com.score.senzors.ui;
 
 import android.app.ActionBar;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import com.score.senzors.R;
 
 /**
@@ -16,11 +22,24 @@ public class SensorDetailsActivity extends FragmentActivity {
     ViewPager Tab;
     TabPagerAdapater TabAdapter;
     ActionBar actionBar;
+    Typeface typefaceThin;
+    TextView actionBarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_details_layout);
+        typefaceThin = Typeface.createFromAsset(this.getAssets(), "fonts/Roboto-Thin.ttf");
+
+        actionBar = getActionBar();
+        // set custom font for
+        //  1. action bar title
+        //  2. other ui texts
+        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        actionBarTitle = (TextView) (this.findViewById(titleId));
+        actionBarTitle.setTextColor(getResources().getColor(R.color.white));
+        actionBarTitle.setTypeface(typefaceThin, Typeface.BOLD);
+
         TabAdapter = new TabPagerAdapater(getSupportFragmentManager());
         Tab = (ViewPager)findViewById(R.id.pager);
         Tab.setOnPageChangeListener(
@@ -31,9 +50,14 @@ public class SensorDetailsActivity extends FragmentActivity {
                         actionBar.setSelectedNavigationItem(position);                    }
                 });
         Tab.setAdapter(TabAdapter);
-        actionBar = getActionBar();
+
         //Enable Tabs on Action Bar
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        setUp();
+    }
+
+    private void setUp() {
         ActionBar.TabListener tabListener = new ActionBar.TabListener() {
             @Override
             public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
@@ -52,9 +76,33 @@ public class SensorDetailsActivity extends FragmentActivity {
             }
         };
 
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("#Location @eranga");
+
+        final TextView t1 = new TextView(this);
+        t1.setText("Location");
+        t1.setTypeface(typefaceThin, Typeface.BOLD);
+        t1.setTextColor(getResources().getColor(R.color.white));
+        t1.setGravity(Gravity.CENTER);
+        t1.setTextSize(19);
+        t1.setLayoutParams(params);
+
+        final TextView t2 = new TextView(this);
+        t2.setText("Sharing");
+        t2.setTypeface(typefaceThin, Typeface.BOLD);
+        t2.setGravity(Gravity.CENTER);
+        t2.setTextColor(getResources().getColor(R.color.white));
+        t2.setLayoutParams(params);
+        t2.setTextSize(19);
+
         //Add New Tab
-        actionBar.addTab(actionBar.newTab().setText("Android").setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab().setText("iOS").setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab().setText("Windows").setTabListener(tabListener));
+        actionBar.addTab(actionBar.newTab().setCustomView(t1).setTabListener(tabListener));
+        actionBar.addTab(actionBar.newTab().setCustomView(t2).setTabListener(tabListener));
+
+
     }
+
 }
