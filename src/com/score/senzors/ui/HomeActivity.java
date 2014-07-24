75515b4716b337f586901010692abbaa963823cf
eranga herath <erangaeb@gmail.com>
2014-07-24 15:51:28 +0530

    refactore home layout added logout button

--- src/com/score/senzors/ui/HomeActivity.java
+++ src/com/score/senzors/ui/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
@@ -14,19 +15,14 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.*;
 import com.score.senzors.application.SenzorApplication;
 import com.score.senzors.pojos.DrawerItem;
 import com.score.senzors.R;
 import com.score.senzors.services.WebSocketService;
 import com.score.senzors.utils.ActivityUtils;
-import com.score.senzors.utils.CryptoUtils;
-import com.score.senzors.utils.PreferenceUtils;
 
 import java.util.ArrayList;
 
@@ -46,6 +42,7 @@ public class HomeActivity extends FragmentActivity {
     // Ui components
     private ListView drawerListView;
     private DrawerLayout drawerLayout;
+    private RelativeLayout drawerContainer;
     private HomeActionBarDrawerToggle homeActionBarDrawerToggle;
 
     // drawer components
@@ -59,14 +56,11 @@ public class HomeActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-
         application = (SenzorApplication)this.getApplication();
 
         initDrawer();
         initDrawerList();
         loadSensors();
-
-        Log.d(TAG, "OnCreate: fragment activity created");
     }
 
     /**
@@ -76,7 +70,6 @@ public class HomeActivity extends FragmentActivity {
         super.onResume();
 
         // register broadcast receiver from here
-        Log.d(TAG, "OnResume: registering broadcast receiver");
         if (dataUpdateReceiver == null) dataUpdateReceiver = new DataUpdateReceiver();
         IntentFilter intentFilter = new IntentFilter(WebSocketService.WEB_SOCKET_DISCONNECTED);
         registerReceiver(dataUpdateReceiver, intentFilter);
@@ -96,8 +89,29 @@ public class HomeActivity extends FragmentActivity {
         super.onPause();
 
         // un-register broadcast receiver from here
-        Log.d(TAG, "OnPause: un-registering broadcast receiver");
         if (dataUpdateReceiver != null) unregisterReceiver(dataUpdateReceiver);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (homeActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -109,14 +123,18 @@ public class HomeActivity extends FragmentActivity {
         getActionBar().setHomeButtonEnabled(true);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerContainer = (RelativeLayout) findViewById(R.id.drawer_container);
+
+        // set custom sign out button
+        TextView signOutTextView = (TextView) findViewById(R.id.sign_out_text);
+        Typeface face = Typeface.createFromAsset(this.getAssets(), "fonts/vegur_2.otf");
+        signOutTextView.setTypeface(face, Typeface.NORMAL);
 
         // set a custom shadow that overlays the main content when the drawer opens
         // set up drawer listener
-        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        //drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         homeActionBarDrawerToggle = new HomeActionBarDrawerToggle(this, drawerLayout);
         drawerLayout.setDrawerListener(homeActionBarDrawerToggle);
-
-        Log.d(TAG, "InitDrawer: drawer initialized");
     }
 
     /**
@@ -141,7 +159,6 @@ public class HomeActivity extends FragmentActivity {
             drawerListView.setAdapter(drawerAdapter);
 
         drawerListView.setOnItemClickListener(new DrawerItemClickListener());
-        Log.d(TAG, "InitDrawerList: initialized drawer list");
     }
 
     /**
@@ -161,73 +178,6 @@ public class HomeActivity extends FragmentActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         homeActionBarDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.home_menu, menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * Called whenever we call invalidateOptionsMenu()
-     */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerListView);
-        menu.findItem(R.id.action_logout).setVisible(!drawerOpen);
-
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-		// The action bar home/up should open or close the drawer.
-		// ActionBarDrawerToggle will take care of this.
-        if (homeActionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        switch (item.getItemId()) {
-            case R.id.action_logout:
-                Log.d(TAG, "OnOptionsItemSelected: logout action clicked");
-//                if(application.getWebSocketConnection().isConnected()) {
-//                    Log.d(TAG, "OnOptionsItemSelected: web socket connected, so disconnect it");
-//                    Log.d(TAG, "OnOptionsItemSelected: force to disconnect web socket");
-//                    ActivityUtils.showProgressDialog(HomeActivity.this, "Disconnecting from senZors...");
-//                    application.setForceToDisconnect(true);
-//                    application.getWebSocketConnection().disconnect();
-//                } else {
-//                    Log.d(TAG, "OnOptionsItemSelected: web socket not connected");
-//                }
-
-//                CryptoUtils cryptoUtils = new CryptoUtils();
-//
-//                if (!PreferenceUtils.isRasKeysSaved(this)) {
-//                    cryptoUtils.initKeys(HomeActivity.this);
-//                } else {
-//                    Log.d(TAG, "Already saved RSA keys");
-//                }
-
-                //cryptoUtils.encryptMessage(HomeActivity.this);
-                //cryptoUtils.decryptMessage(HomeActivity.this);
-
-                break;
-        }
-
-        // Handle your other action bar items.
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -265,7 +215,7 @@ public class HomeActivity extends FragmentActivity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             // Highlight the selected item, update the title, and close the drawer
             // update selected item and title, then close the drawer
-            drawerLayout.closeDrawer(drawerListView);
+            drawerLayout.closeDrawer(drawerContainer);
 
             //  reset content in drawer list
             for(DrawerItem drawerItem: drawerItemList) {
@@ -302,22 +252,6 @@ public class HomeActivity extends FragmentActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.main, sensorListFragment);
         transaction.commit();
-        Log.d(TAG, "LoadSensors: load sensor list fragment");
-    }
-
-    /**
-     * Load friends fragment
-     */
-    private void loadFriends() {
-        FriendList friendListFragment = new FriendList();
-
-        // fragment transitions
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack so the user can navigate back
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.main, friendListFragment);
-        transaction.commit();
-        Log.d(TAG, "LoadFriends: load friend list fragment");
     }
 
     /**
@@ -330,7 +264,6 @@ public class HomeActivity extends FragmentActivity {
         HomeActivity.this.finish();
 
         HomeActivity.this.overridePendingTransition(R.anim.left_in, R.anim.right_out);
-        Log.d(TAG, "SwitchToLogin: switched to login activity");
     }
 
     /**
